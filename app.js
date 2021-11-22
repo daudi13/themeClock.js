ab3ec544dc718ed9f76252b026983e7d31d364e0
daudi13 <davouma13@gmail.com>
2021-11-22 22:57:45 +0300

    commit changes

--- app.js
+++ app.js
@@ -7,17 +7,19 @@ const thedate = document.querySelector('.date');
 const html = document.querySelector('html')
 
 theBtn.addEventListener('click', function (e) {
-    if (theBtn.classList.contains('dark')) {
+    if (html.classList.contains('dark')) {
         html.classList.remove('dark');
         e.target.innerText = 'Dark Mode';
-        theBtn.style.backgroundColor = `#fff`;
-        theHour.style.backgroundColor = `#fff`;
-        theMinute.style.backgroundColor = `#fff`;
-    } else {
-        html.classList.add('dark');
-        e.target.innerText = 'Light Mode';
         theBtn.style.backgroundColor = `#000`;
         theHour.style.backgroundColor = `#000`;
         theMinute.style.backgroundColor = `#000`;
+        theBtn.style.color = `#fff`;
+    } else {
+        html.classList.add('dark');
+        e.target.innerText = 'Light Mode';
+        theBtn.style.backgroundColor = `#fff`;
+        theHour.style.backgroundColor = `#fff`;
+        theMinute.style.backgroundColor = `#fff`;
+        theBtn.style.color = `#000`;
     }
 });
