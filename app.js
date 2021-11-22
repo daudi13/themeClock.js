a3b859288fd64f6c93fa7364f655ff34c331144f
daudi13 <davouma13@gmail.com>
2021-11-22 22:31:11 +0300

    commit changes

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const thedate = document.querySelector('.date');
 const html = document.querySelector('html')
 
 theBtn.addEventListener('click', function () {
-    if (!theBtn.classList.contains('dark')) {
+    if (theBtn.classList.contains('dark')) {
         html.classList.add('dark');
         theBtn.innerHTML = `Light Mode`;
         theBtn.classList.add('dark');
